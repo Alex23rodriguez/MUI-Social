b906c88466697051657febc045f2a388fc180c74
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-14 17:35:26 -0500

    feat: added main components' boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Feed } from "./components/Feed";
+import { Rightbar } from "./components/Rightbar";
+import { Sidebar } from "./components/Sidebar";
+
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </div>
+  );
 }
 
 export default App;
