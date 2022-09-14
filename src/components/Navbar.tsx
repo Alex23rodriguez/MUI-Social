5a69e9745c20f849cfab6c4c2edd8d3da7135002
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-14 18:08:38 -0500

    feat: added a basic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,19 @@
+import { AppBar, styled, Toolbar, Typography } from "@mui/material";
+import CycloneIcon from "@mui/icons-material/Cyclone";
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <CycloneIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          MUI Social
+        </Typography>
+      </StyledToolbar>
+    </AppBar>
+  );
 };
