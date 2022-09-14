acab7a3fc2382188518bfccec4769920a2715d03
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-14 18:22:23 -0500

    feat: added search field to navbar and some icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,30 @@
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  InputBase,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import CycloneIcon from "@mui/icons-material/Cyclone";
+import MailIcon from "@mui/icons-material/Mail";
+import Notifications from "@mui/icons-material/Notifications";
+
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+}));
 
 export const Navbar = () => {
   return (
@@ -13,6 +34,18 @@ export const Navbar = () => {
         <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
           MUI Social
         </Typography>
+
+        <Search>
+          <InputBase placeholder="search" />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+        </Icons>
       </StyledToolbar>
     </AppBar>
   );
