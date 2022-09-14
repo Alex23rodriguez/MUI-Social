c5512544e15784c87c104e55553b71d572c9c6e0
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-14 17:26:25 -0500

    Undo recent commits for a clean start on the project Revert from "feat: how to add a theme provider" to "feat: testing out button variants, icons and typography"

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,5 @@
-import { Button, styled, SxProps, Typography } from "@mui/material";
-import SettingsIcon from "@mui/icons-material/Settings";
-import { Add } from "@mui/icons-material";
-
 function App() {
-  const BlueButton = styled(Button)(({ theme }) => ({
-    backgroundColor: theme.palette.info.main,
-    color: "#888",
-    margin: 5,
-    "&:hover": {
-      backgroundColor: "lightblue",
-    },
-    "&:disabled": {
-      backgroundColor: "gray",
-      color: "white",
-    },
-  }));
-  return (
-    <div className="App">
-      <Button variant="text">Text</Button>
-      <Button
-        startIcon={<SettingsIcon />}
-        variant="contained"
-        color="secondary"
-        size="small"
-      >
-        Contained
-      </Button>
-      <Button variant="contained" color="success" startIcon={<Add />}>
-        Add new post
-      </Button>
-      <Button variant="outlined" disabled>
-        Outlined
-      </Button>
-      <Typography variant="h1" component="h2">
-        It uses h1 tag but it is an h2
-      </Typography>
-      <BlueButton>My unique button</BlueButton>
-      <BlueButton>anothe button</BlueButton>
-    </div>
-  );
+  return <div className="App">Hello world!</div>;
 }
 
 export default App;
