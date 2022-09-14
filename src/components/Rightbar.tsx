83651ad70d26431f544ca2e2bea86c1b469e7a4e
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-14 17:53:45 -0500

    feat: responsive sidebars!

--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -2,7 +2,12 @@ import { Box } from "@mui/material";
 
 export const Rightbar = () => {
   return (
-    <Box bgcolor="lightcoral" flex={2} p={2}>
+    <Box
+      bgcolor="lightcoral"
+      flex={2}
+      p={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
       Rightbar
     </Box>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,12 @@ import { Box } from "@mui/material";
 
 export const Sidebar = () => {
   return (
-    <Box bgcolor="skyblue" flex={1} p={2}>
+    <Box
+      bgcolor="skyblue"
+      flex={1}
+      p={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
       Sidebar
     </Box>
   );
