2c242660ff0c2d917008f7334d2b896e7ac44f96
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-14 13:50:26 -0500

    feat: testing out button variants, icons and typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { Button, Typography } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { Add } from "@mui/icons-material";
+
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Contained
+      </Button>
+      <Button variant="contained" color="success" startIcon={<Add />}>
+        Add new post
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="h2">
+        It uses h1 tag but it is an h2
+      </Typography>
+    </div>
+  );
 }
 
 export default App;
