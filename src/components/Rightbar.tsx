b967c11d89d9f48291a0e8ac8809a8d85d8006c0
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-14 20:54:31 -0500

    feat: added 'latest photos' to rightbar

--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -1,10 +1,65 @@
 import { Avatar, AvatarGroup, Box, Typography } from "@mui/material";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+];
 
 export const Rightbar = () => {
   return (
     <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      <Box position="fixed">
-        <Typography fontWeight={100}>Online Friends</Typography>
+      <Box position="fixed" p={3}>
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
         <AvatarGroup max={4}>
           <Avatar alt="Remy Sharp" src="http://placeimg.com/301/301/people" />
           <Avatar
@@ -18,6 +73,29 @@ export const Rightbar = () => {
             src="http://placeimg.com/305/305/people"
           />
         </AvatarGroup>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Latest Photos
+        </Typography>
+        <ImageList
+          // sx={{ width: "100%", height: "auto" }}
+          cols={2}
+          rowHeight={120}
+          gap={5}
+        >
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Latest Conversations
+        </Typography>
       </Box>
     </Box>
   );
