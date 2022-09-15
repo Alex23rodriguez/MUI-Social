f55b733b6171601b979a553701ccca2550fb7b40
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-14 20:13:55 -0500

    feat: move posts to own component and added a couple

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,61 +1,35 @@
 import { Box } from "@mui/material";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import ShareIcon from "@mui/icons-material/Share";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import Checkbox from "@mui/material/Checkbox";
+import { Post } from "./Post";
+
+const posts = [
+  {
+    user: "A",
+    title: "Paella with chicken",
+    img: "https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?cs=srgb&dl=pexels-rajesh-tp-1624487.jpg&fm=jpg",
+    desc: `This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.`,
+  },
+  {
+    user: "R",
+    title: "Pancakes!",
+    img: "https://images.pexels.com/photos/357573/pexels-photo-357573.jpeg?cs=srgb&dl=pexels-pixabay-357573.jpg&fm=jpg",
+    desc: `You know how to make Pancakes. If you don't google it`,
+  },
+  {
+    user: "B",
+    title: "Best Pizza ever",
+    img: "https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg?cs=srgb&dl=pexels-pixabay-315755.jpg&fm=jpg",
+    desc: `This is how to make the best pizza ever: I can't say lol`,
+  },
+];
 
 export const Feed = () => {
   return (
     <Box flex={4} p={2}>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
-        />
-        <CardMedia
-          component="img"
-          height="20%"
-          image="https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress"
-          alt="Paella dish"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <Checkbox
-            aria-label="add to favorites"
-            icon={<FavoriteBorder />}
-            checkedIcon={<FavoriteIcon sx={{ color: "lightcoral" }} />}
-          />
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
+      <Post {...posts[0]} />
+      <Post {...posts[1]}/>
+      <Post {...posts[2]}/>
     </Box>
   );
 };
---
+++ src/components/Post.tsx
@@ -0,0 +1,62 @@
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import ShareIcon from "@mui/icons-material/Share";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Checkbox from "@mui/material/Checkbox";
+import { FC } from "react";
+
+export const Post: FC<{
+  user: string;
+  title: string;
+  img: string;
+  desc: string;
+}> = (props) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {props.user}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.title}
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="20%"
+        image={props.img}
+        alt={props.title}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {props.desc}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Checkbox
+          aria-label="add to favorites"
+          icon={<FavoriteBorder />}
+          checkedIcon={<FavoriteIcon sx={{ color: "lightcoral" }} />}
+        />
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
