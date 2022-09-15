0de4026ae9869c89577f7c95b99f7f3c58c5dc76
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-09-14 19:46:16 -0500

    feat: added (non-functional) dark mode switch to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,11 +5,13 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Switch,
 } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import PagesIcon from "@mui/icons-material/Pages";
 import GroupsIcon from "@mui/icons-material/Group";
 import ProfileIcon from "@mui/icons-material/Person";
+import { ModeNight } from "@mui/icons-material";
 
 export const Sidebar = () => {
   return (
@@ -45,6 +47,14 @@ export const Sidebar = () => {
               <ProfileIcon />
             </ListItemIcon>
             <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#darkmode">
+            <ListItemIcon>
+              <ModeNight />
+            </ListItemIcon>
+            <Switch />
           </ListItemButton>
         </ListItem>
       </List>
